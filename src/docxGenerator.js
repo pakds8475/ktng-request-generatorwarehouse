d6a8b66032dda0fb17fea276cf670b00a27b5eb0
pakds8475 <81705389+pakds8475@users.noreply.github.com>
2026-06-05 14:01:11 +0500

    Update docxGenerator.js

--- src/docxGenerator.js
+++ src/docxGenerator.js
@@ -458,7 +458,7 @@ createGoodsTable(data),
 
 paragraph(`Иная информация о грузе: ${safeValue(data.cargoInfo)}`, {
   size: 24,
-  spacing: { before: 220, after: 1200 },
+  spacing: { before: 220, after: 2000 },
 }),
 
 createSignatureTable(data),
